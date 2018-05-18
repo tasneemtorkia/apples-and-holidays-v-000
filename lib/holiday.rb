a1f74eda944e47b289e2e4fd588be9391b4f3ad2
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-05-18 12:32:03 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,6 +73,7 @@ def all_holidays_with_bbq(holiday_hash)
 holiday_hash.each do |season, holiday|
 holiday.each do |holiday, supplies|
   if supplies.include?("BBQ")
+    bbq_hol << holiday
     
 end
 end 
